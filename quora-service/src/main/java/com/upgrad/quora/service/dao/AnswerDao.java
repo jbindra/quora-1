9c67d6cd8390ca7a302c0a1ef35b93adf952be41
jbindra <57809385+jbindra@users.noreply.github.com>
2020-02-17 18:41:23 +0530

    Data access

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -0,0 +1,25 @@
+package com.upgrad.quora.service.dao;
+
+
+import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class AnswerDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    //This Method gets all the answer stores for a particular Question using question as the parameter.
+    //Returns a list of answer.
+    public List<AnswerEntity> getAllAnswerToQuestion(QuestionEntity questionEntity){
+        List<AnswerEntity> answerEntities = entityManager.createNamedQuery("getAllAnswerToQuestion",AnswerEntity.class).setParameter("question",questionEntity).getResultList();
+        return answerEntities;
+    }
+}
